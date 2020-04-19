6bcedab2a9b6edea21948db6d27c7d992c942e6e
asyaasha <asya.asha@gmail.com>
2020-04-19 11:24:11 -0700

    fix initial orientation

--- src/main/java/controller/GamePlayController.java
+++ src/main/java/controller/GamePlayController.java
@@ -47,12 +47,13 @@ public class GamePlayController {
         view.setStatusMessage(simulation.status);
     }
 
-    public void stepForward() throws Exception {
+    public void stepForward(JButton[][] squares) throws Exception {
         simulation = db.loadSimulationState(user, false);
         while (!simulation.status.equals(END_STATUS)) {
             simulation.stepSimulation();
             db.saveAndUploadState(simulation, user);
             simulation = db.loadSimulationState(user, false);
+            renderMap(squares);
             view.setStatusMessage(simulation.status);
         }
     }
@@ -72,8 +73,6 @@ public class GamePlayController {
             for(int x = 1; x < squares[1].length; x++) {
                 if (simulation.getVirtualizedMap().getSpaceLayout()[y][x].getStarFieldContents() == Content.DRONE) {
                     String orientation = String.valueOf(simulation.getVirtualizedMap().getSpaceLayout()[y][x].getOccupantDrone().getDroneOrientation());
-                    System.out.println("orientation");
-                    System.out.println(orientation);
                     squares[y][x].setIcon(view.droneIconsMap.get(orientation));
                     squares[y][x].setText("");
                 } else if (simulation.getVirtualizedMap().getSpaceLayout()[y][x].getStarFieldContents() == Content.EMPTY) {
